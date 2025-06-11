f6fd1c40ebd610eb632a7dc174b38c979fe317da
friskodomi <frisko.dominika2002@gmail.com>
2025-06-11 23:23:02 +0200

    deleting reminders as redundant

--- ebreszto-alkalmazas/views/reminders_view.py
+++
@@ -1,6 +0,0 @@
-from PySide6.QtWidgets import QWidget
-import ui_files.rc_icons
-
-class RemindersView:
-    def __init__(self, remindersPage_widget):
-        self.remindersPage = remindersPage_widget
